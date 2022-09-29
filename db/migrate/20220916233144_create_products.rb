1619c1c5116a0c2ee67d1bba8b1176447b71c502
mluthfij <luthfujuliansyah48@gmail.com>
2022-09-29 10:07:07 +0700

    edit decimal to float

--- db/migrate/20220916233144_create_products.rb
+++ db/migrate/20220916233144_create_products.rb
@@ -3,8 +3,8 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.string :name
       # t.integer :price
-      # t.float :price
-      t.decimal :price, precision: 5, scale: 2
+      t.float :price
+      # t.decimal :price, precision: 5, scale: 2
       t.integer :quantity
       t.text :description
 
