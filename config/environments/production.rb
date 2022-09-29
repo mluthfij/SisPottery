e67a8e9a07d9589a7bf753d2f564420efa75be83
mluthfij <luthfujuliansyah48@gmail.com>
2022-09-29 09:24:05 +0700

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,24 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings send email
+  
+  # 
+  # config.action_mailer.default_url_options = {host: "sis-pottery.herokuapp.com", protocol: "https"}
+  config.action_mailer.default_url_options = {host: "sis-pottery.herokuapp.com"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  # 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.dig(:google_smtp, :email),
+    password:             Rails.application.credentials.dig(:google_smtp, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
