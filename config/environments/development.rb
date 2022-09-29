001c79ed6726a2ace10d6cde8d5c836e6a91c1af
mluthfij <luthfujuliansyah48@gmail.com>
2022-09-29 09:06:53 +0700

    edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {host: "sis-pottery.herokuapp.com", protocol: "https"}
+  
   # 
   config.action_mailer.delivery_method = :smtp
   # SMTP Settings for mailcatcher gem.
