f157c5cabfc4b06cb1e68ff2e35f284e5bc3cdec
mluthfij <luthfujuliansyah48@gmail.com>
2022-10-05 17:17:02 +0700

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -140,6 +140,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Set Action Cable server url for consumer connection
-  config.action_cable.url = "wss://sis-pottery.herokuapp.com/cable"
+  # config.action_cable.url = "wss://sis-pottery.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://sis-pottery.herokuapp.com/cable" 
   config.action_cable.allowed_request_origins = ['https://sis-pottery.herokuapp.com', /http:\/\/sis-pottery.herokuapp.com.*/ ]
 end
