f0e4dbe2d8950b79efe2a59fb29e79d570c22d09
mluthfij <luthfujuliansyah48@gmail.com>
2022-10-20 14:29:37 +0700

    edit home and about page

--- app/models/homepagess.rb
+++ app/models/homepagess.rb
@@ -1,4 +1,6 @@
 class Homepagess < ApplicationRecord
-    validates :description, length: { in: 150..215 }
+    # validates :description, length: { in: 150..215 }
+    validates :brand_name, presence: true, length: { maximum: 78 }
+    validates :description, presence: true, length: { maximum: 27 }
     has_many_attached :covers
 end
