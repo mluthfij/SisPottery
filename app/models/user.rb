9e70af8c7b69f43e9fb8225331e5f680ff88e6c5
mluthfij <luthfujuliansyah48@gmail.com>
2022-10-31 21:21:24 +0700

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        #  , :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 
          acts_as_voter
 
