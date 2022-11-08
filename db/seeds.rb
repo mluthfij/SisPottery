b9b8e3d40438a7adeef071d913c11a6833d1d8b8
mluthfij <luthfujuliansyah48@gmail.com>
2022-11-08 14:16:54 +0700

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,26 @@
 # Product.destroy_all
 # friendly_id_slugs.destroy_all
 
+User.update(username: 'admin1', email: 'belajarrubyonrails12@gmail.com', password: '111111', admin: true, user_id:2)
+
 
 # if User.count < 2
-    # User.create(username: 'admin', email: 'belajarrubyonrails12@gmail.com', password: '111111', admin: true)
-    # User.create(username: 'luthfi', email: 'luthfujuliansyah48@gmail.com', password: '111111')
+#     User.create(username: 'admin', email: 'belajarrubyonrails12@gmail.com', password: '111111', admin: true)
+#     User.create(username: 'luthfi', email: 'luthfujuliansyah48@gmail.com', password: '111111')
 # end
 
-# if Product.count < 100
-#     (1..100).each do |i|
-#         Product.create(name: "Product #{i}", user_id: 1, price: rand(15..100), created_at: Time.now - i.days, updated_at: Time.now - i.days)
-#     end
-# end
+if Product.count < 100
+    (1..100).each do |i|
+        Product.create(name: "Product #{i}", user_id: 2, price: rand(15..100), created_at: Time.now - i.days, updated_at: Time.now - i.days)
+    end
+end
 
-# if Keep.count < 100
-#     (1..100).each do |k|
-#         # Keep.create(product_name: "Product #{i}", user_id: 1, product_price: rand(15..100), point: i, quantity: rand(1..50), created_at: Time.now - i.days, updated_at: Time.now - i.days)
-#         # Keep.create(product_name: "Product #{i}", user_id: 1, product_price: rand(15..100), point: i, quantity: rand(1..50), created_at: Time.now - i.days, updated_at: Time.now - i.days)
-#         Keep.create(bucket_id: 1, customer: "luthfi", product_name: "Product #{k}", product_price: rand(500000..2500000), quantity: rand(5..50), total_price: rand(500000..2500000), order_start: "0026-10-22", order_end: "2022-10-05", point: k, product_id: k, user_id: 2, created_at: Time.now - k.days, updated_at: Time.now - k.days)
-#     end
-# end
+if Keep.count < 100
+    (1..100).each do |k|
+        # Keep.create(product_name: "Product #{i}", user_id: 1, product_price: rand(15..100), point: i, quantity: rand(1..50), created_at: Time.now - i.days, updated_at: Time.now - i.days)
+        # Keep.create(product_name: "Product #{i}", user_id: 1, product_price: rand(15..100), point: i, quantity: rand(1..50), created_at: Time.now - i.days, updated_at: Time.now - i.days)
+        Keep.create(bucket_id: 2, customer: "luthfi", product_name: "Product #{k}", product_price: rand(500000..2500000), quantity: rand(5..50), total_price: rand(500000..2500000), order_start: "0026-10-22", order_end: "2022-10-05", point: k, product_id: k, user_id: 1, created_at: Time.now - k.days, updated_at: Time.now - k.days)
+    end
+end
 
 # Keep.create(bucket_id: 1, customer: "luthfi", product_name: "Product 100", product_price: 54, quantity: 3, total_price: 486, order_start: "0026-10-22", order_end: "2022-10-05", point: 200, user_id: 2)
