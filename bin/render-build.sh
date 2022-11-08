417682841bcb61908e50cfb35f599235e18d1865
mluthfij <luthfujuliansyah48@gmail.com>
2022-11-08 15:10:49 +0700

    add data seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,7 @@ set -o errexit
 bundle install --with development test
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:purge
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
