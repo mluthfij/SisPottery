c4f0870804b9660550012373a3ddba8f8fa0abb3
mluthfij <luthfujuliansyah48@gmail.com>
2022-11-10 05:48:48 +0700

    set credential

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable, 
-        :timeoutable, :confirmable
+        :timeoutable
+        # , :confirmable
 
         acts_as_voter
 
