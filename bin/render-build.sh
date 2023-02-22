a3998acacc4a3a6005fe2ab9b13438f74dffa5e3
Muhammad Luthfi Juliansyah <62416061+mluthfij@users.noreply.github.com>
2023-02-22 10:37:41 +0700

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,5 @@ bundle install --with development test
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-# bundle exec rails db:seed
+bundle exec rails db:reset
+bundle exec rails db:seed
